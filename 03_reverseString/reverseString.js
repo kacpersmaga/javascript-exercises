22e96797c6db4a94653e50acbd8e36aa5b69c9c5
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-05 00:26:30 +0200

    another solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -3,12 +3,7 @@
  * @param {string} word 
  */
 const reverseString = function(word) {
-    let newWord=""
-    
-for(let i=word.length-1;i>=0;i--){
-    newWord=newWord+word[i]
-}
-return newWord
+    return word.split("").reverse().join("")
 };
 
 // Do not edit below this line
