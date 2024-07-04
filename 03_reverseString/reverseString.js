06fd5b1914f0ad0e30865e093de27a1a5491a748
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-05 00:19:51 +0200

    reverses single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+/**
+ * 
+ * @param {string} word 
+ */
+const reverseString = function(word) {
+    let newWord=""
+    
+for(let i=word.length-1;i>=0;i--){
+    newWord=newWord+word[i]
+}
+return newWord
 };
 
 // Do not edit below this line
