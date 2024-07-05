f62b14692136cf078ea1d53f714abbdd72baf564
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-05 02:09:29 +0200

    made it remove number from arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, numberToRemove) {
+    for(let i=1;i<arguments.length;i++){
+
+        while(array.indexOf(arguments[i])!=-1){
+            index = array.indexOf(arguments[i])
+            array.splice(index, 1)
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
