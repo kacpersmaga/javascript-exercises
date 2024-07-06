6c2a42391378b810af94acea97ba1c032b9cdf0d
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-06 21:06:11 +0200

    made summing funciton

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(startOfTheRange, endOfTheRange) {
+    if(startOfTheRange<0||endOfTheRange<0||typeof startOfTheRange != 'number'||typeof endOfTheRange != 'number')
+        return "ERROR"
+
+    if(endOfTheRange<startOfTheRange)
+        [startOfTheRange,endOfTheRange]=[endOfTheRange,startOfTheRange]
+
+    let suma = 0
+    for(let i=startOfTheRange;i<=endOfTheRange;i++)
+        suma+=i
+
+    return suma
 
 };
 
