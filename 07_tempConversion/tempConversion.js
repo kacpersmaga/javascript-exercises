8425da18f13d47d4f5422735861fd7c3752f1d72
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-07 00:21:34 +0200

    made temperature conversion from Celcius to Fahrenheit and the other way

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FahrenheitTemperature) {
+  CelciusTemperature = Math.round(((FahrenheitTemperature - 32) * 5 / 9) * 10) / 10
+  
+  return CelciusTemperature
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CelsiusTemperature) {
+  FahrenheitTemperature = Math.round((CelsiusTemperature * 9/5 + 32) * 10) / 10
+
+  return FahrenheitTemperature
 };
 
 // Do not edit below this line
