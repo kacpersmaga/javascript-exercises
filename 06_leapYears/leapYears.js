3be2af69ff4996e2b6a2f13cfcd47c91853bf8ed
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-06 21:24:10 +0200

    made a function that checks if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    // Divisible by 4
+    if(year%4===0)
+        if(year%100!=0)
+            return true
+        else if(year%400===0)
+            return true
+
+        return false
+
+    // Divisible by 400
+
 
 };
 
