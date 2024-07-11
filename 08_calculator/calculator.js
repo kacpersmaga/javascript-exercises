838a9fe37c3616c55387a542678ab3d1ba41c8ce
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-12 00:31:53 +0200

    wrote all functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,38 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array.length===0)
+    return 0;
+	return array.reduce((total, currentNumber) => total+currentNumber);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+ return numbers.reduce((total, currentNum) => total*currentNum, 1 )
 };
 
-const power = function() {
-	
+const power = function(num1, power) {
+  let multiplier = num1;
+  if(power == 0)
+    return 1;
+	while(power>1){
+    num1=num1*multiplier;
+    power--;
+  }
+  return num1;
 };
 
-const factorial = function() {
+const factorial = function(lastNum) {
+let sum = 1;
+  for(let i=1;i<=lastNum;i++){
+    sum = sum*i
+  }
+  return sum;
 	
 };
 
