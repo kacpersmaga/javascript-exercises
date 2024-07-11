3bd97467e5625800fc9ec2462d9a56be0dc5b82b
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-12 01:17:27 +0200

    created findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,9 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.sort((a,b) =>{
+        const ageA = (a.yearOfDeath || new Date().getFullYear()) - a.yearOfBirth;
+        const ageB = (b.yearOfDeath || new Date().getFullYear()) - b.yearOfBirth;
+        return ageA < ageB ? 1:-1;
+    })[0];
 
 };
 
