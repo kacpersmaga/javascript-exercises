119cbb0d8e1a9e88b0ce47a06b48b7e7a9461d7d
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-12 00:47:13 +0200

    made a palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const cleaned = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+ const reversed = cleaned.split('').reverse().join('');
+ return cleaned===reversed
 };
 
 // Do not edit below this line
