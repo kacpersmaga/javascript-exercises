1b5ce612b434706bfd7f2028f34e372adc4bfe7a
kacpersmaga <kacper.smaga@microsoft.wsei.edu.pl>
2024-07-12 01:05:48 +0200

    write a fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(ammount) {
+ let num1 = 0;
+ let num2 = 1;
+  ammount = parseInt(ammount);
+ if(ammount<0)
+    return "OOPS"
+ if(ammount===0)
+    return 0;
+ for(let i=0;i<ammount;i++){
+    [num1, num2] = [num1 + num2, num1]
+ }
+ if(num1>num2)
+    return num1
+else return num2;
 };
 
 // Do not edit below this line
